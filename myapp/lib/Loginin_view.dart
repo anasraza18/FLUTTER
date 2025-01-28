0b2f8b50f4f206b0f51a9d8dc7414e402df9fb56
anasraza18 <manasraza18@gmail.com>
2025-01-28 12:27:08 +0500

    update

---
+++ myapp/lib/Loginin_view.dart
@@ -0,0 +1,11 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
