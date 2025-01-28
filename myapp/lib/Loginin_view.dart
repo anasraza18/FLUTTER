8c946fb7f07b82af1e16241791bf1f879ed20c47
anasraza18 <manasraza18@gmail.com>
2025-01-28 12:44:51 +0500

    update

--- myapp/lib/Loginin_view.dart
+++ myapp/lib/Loginin_view.dart
@@ -1,4 +1,3 @@
-// ignore: file_names
 import 'package:flutter/material.dart';
 
 class LoginView extends StatelessWidget {
@@ -6,6 +5,12 @@ class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: const Text("Anas"),
+      ),
+      body: const Text("App Body"),
+    );
   }
 }
