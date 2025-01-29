49f6fa08ddecb1bb34b67d71f960f5e490edda37
anasraza18 <manasraza18@gmail.com>
2025-01-29 14:28:13 +0500

    update

--- myapp/lib/Loginin_view.dart
+++ myapp/lib/Loginin_view.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class LoginView extends StatelessWidget {
@@ -13,6 +15,24 @@ class LoginView extends StatelessWidget {
               image: DecorationImage(
                 image: AssetImage("assets/images/ai.jpg"),
                 fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20), // Corrected BorderRadius
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  width: 320,
+                  height: 500,
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.2),
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.white.withOpacity(0.3)),
+                  ),
+                ),
               ),
             ),
           ),
