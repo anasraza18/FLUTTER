534aef0438a161e95f7bd69ce9e780d7e0739729
anasraza18 <manasraza18@gmail.com>
2025-01-29 10:53:59 +0500

    update

--- myapp/lib/Loginin_view.dart
+++ myapp/lib/Loginin_view.dart
@@ -10,48 +10,8 @@ class LoginView extends StatelessWidget {
         backgroundColor: Colors.orange,
         title: Text("Anas"),
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 10),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  //margin: EdgeInsets.all(10),
-                  //padding: EdgeInsets.all(10),
-                  color: Colors.red,
-                  width: 150,
-                  height: 150,
-                ),
-                Container(
-                  color: Colors.red,
-                  width: 150,
-                  height: 150,
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  color: Colors.red,
-                  width: 150,
-                  height: 150,
-                ),
-                Container(
-                  color: Colors.red,
-                  width: 150,
-                  height: 150,
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+      body: Image.network(
+          "https://scontent.fkhi12-1.fna.fbcdn.net/v/t39.30808-6/470588988_2314894895530378_1865305021858890164_n.jpg?stp=dst-jpg_s960x960_tt6&_nc_cat=110&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeEl_3KwOY89M5HfSr5PTG3lqtgpAUw8DWOq2CkBTDwNY04o-pPVsOT5MwWPtqxo0kQ_L14G_TdNZeWzwrgMaQ8H&_nc_ohc=tBnj5yWtUnoQ7kNvgGlMChi&_nc_zt=23&_nc_ht=scontent.fkhi12-1.fna&_nc_gid=AYT1EUx1jyaBQgRUC0QxfaD&oh=00_AYBzF94E6j_UeRHRDRbfCLO3eepD0wVbPN8FzB19TugLRA&oe=679FACEE"),
     );
   }
 }
