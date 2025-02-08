b2bd358c1441414c6684c8e929ecbc0347fa24ff
anasraza18 <manasraza18@gmail.com>
2025-02-08 18:33:59 +0500

    update

---
+++ myapp2/lib/Widgets/productsWidg.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+class Productswidg extends StatelessWidget {
+  List data = [
+    {'product': 'product 1', 'price': '223', 'image': 'assets/images/21.webp'},
+    {'product': 'product 2', 'price': '113', 'image': 'assets/images/2.webp'},
+    {'product': 'product 3', 'price': '653', 'image': 'assets/images/3.webp'},
+    {'product': 'product 4', 'price': '983', 'image': 'assets/images/6.webp'},
+    {'product': 'product 5', 'price': '223', 'image': 'assets/images/15.webp'},
+    {'product': 'product 6', 'price': '113', 'image': 'assets/images/4.webp'},
+    {'product': 'product 7', 'price': '653', 'image': 'assets/images/7.webp'},
+    {'product': 'product 8', 'price': '983', 'image': 'assets/images/18.webp'},
+    {'product': 'product 9', 'price': '653', 'image': 'assets/images/9.webp'},
+    {'product': 'product 10', 'price': '983', 'image': 'assets/images/16.webp'},
+  ];
+  Productswidg({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 5,
+          crossAxisSpacing: 10,
+          mainAxisExtent: MediaQuery.of(context).size.height * 0.30),
+      padding: EdgeInsets.all(10),
+      itemCount: data.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+          color: Colors.white,
+          shadowColor: Colors.black,
+          elevation: 4,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Column(
+            children: [
+              Expanded(
+                child: ClipRRect(
+                  child: Image.asset(data[index]['image'], fit: BoxFit.cover),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Column(
+                //mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        data[index]['product'],
+                        style: TextStyle(color: Colors.black, fontSize: 14),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        "/~~Rs.${data[index]['price']}  ",
+                        style: TextStyle(color: Colors.black, fontSize: 14),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.shopping_bag),
+                      ),
+                      // Icon(Icons.shopping_bag),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
--- myapp2/lib/productList.dart
+++ myapp2/lib/productList.dart
@@ -1,26 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:myapp2/Widgets/productsWidg.dart';
 
 // ignore: must_be_immutable
 class ProductList extends StatelessWidget {
-  List data = [
-    {'product': 'product 1', 'price': '223', 'image': 'assets/images/21.webp'},
-    {'product': 'product 2', 'price': '113', 'image': 'assets/images/2.webp'},
-    {'product': 'product 3', 'price': '653', 'image': 'assets/images/3.webp'},
-    {'product': 'product 4', 'price': '983', 'image': 'assets/images/6.webp'},
-    {'product': 'product 5', 'price': '223', 'image': 'assets/images/15.webp'},
-    {'product': 'product 6', 'price': '113', 'image': 'assets/images/4.webp'},
-    {'product': 'product 7', 'price': '653', 'image': 'assets/images/7.webp'},
-    {'product': 'product 8', 'price': '983', 'image': 'assets/images/18.webp'},
-    {'product': 'product 9', 'price': '653', 'image': 'assets/images/9.webp'},
-    {'product': 'product 10', 'price': '983', 'image': 'assets/images/16.webp'},
-  ];
   ProductList({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFFFFDAB9),
+        backgroundColor: Color(0xFFFFF0E1),
         // title: Text(
         //   "Products",
         //   style: TextStyle(
@@ -35,19 +24,25 @@ class ProductList extends StatelessWidget {
               children: [
                 // arrow back and circle avatar
                 Container(
-                  color: Color(0xFFFFDAB9),
+                  color: Color(0xFFFFF0E1),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey.withOpacity(0.7),
+                          borderRadius: BorderRadius.circular(5),
+                        ),
                         padding: EdgeInsets.all(3),
-                        //margin: EdgeInsets.all(2),
+                        margin: EdgeInsets.all(15),
+                        height: 40,
+                        width: 40,
                         child: IconButton(
                             onPressed: () {}, icon: Icon(Icons.arrow_back)),
                       ),
                       Container(
                         padding: EdgeInsets.all(3),
-                        margin: EdgeInsets.all(4),
+                        margin: EdgeInsets.all(15),
                         child: CircleAvatar(
                           backgroundImage: AssetImage('assets/images/anas.jpg'),
                         ),
@@ -59,21 +54,23 @@ class ProductList extends StatelessWidget {
             ),
             // serach bar
             Container(
-              color: Color(0xFFFFDAB9),
+              color: Color(0xFFFFF0E1),
               padding: EdgeInsets.all(10),
               child: TextField(
                 decoration: InputDecoration(
                     border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25),
+                      borderRadius: BorderRadius.circular(15),
                     ),
+                    fillColor: Colors.white,
+                    filled: true,
                     prefixIcon: Icon(Icons.search),
                     labelText: "Search"),
               ),
             ),
             // large main image
             Container(
-              color: Color(0xFFFFDAB9),
-              //padding: EdgeInsets.all(10),
+              color: Color(0xFFFFF0E1),
+              padding: EdgeInsets.all(13),
               child: ClipRRect(
                 child: Image.asset(
                   'assets/images/23.webp',
@@ -87,29 +84,57 @@ class ProductList extends StatelessWidget {
             Column(
               children: [
                 Container(
-                  color: Color(0xFFFFDAB9),
+                  color: Color(0xFFFFF0E1),
                   padding: EdgeInsets.all(10),
                   child: Row(
                     children: [
                       Container(
-                        color: Colors.grey,
                         padding: EdgeInsets.all(10),
-                        child: Text("All"),
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 209, 137, 110),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        height: 40,
+                        width: 50,
+                        child: Center(
+                          child: Text(
+                            "All",
+                            style: TextStyle(),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
                       ),
                       Container(
-                        color: Colors.grey,
                         padding: EdgeInsets.all(10),
-                        child: Text("All"),
+                        child: Center(child: Text("Ice Cube")),
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
                       ),
                       Container(
-                        color: Colors.grey,
                         padding: EdgeInsets.all(10),
-                        child: Text("All"),
+                        child: Center(child: Text("Cones")),
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
                       ),
                       Container(
-                        color: Colors.grey,
                         padding: EdgeInsets.all(10),
-                        child: Text("All"),
+                        child: Center(child: Text("Shakes")),
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
                       ),
                     ],
                   ),
@@ -118,95 +143,8 @@ class ProductList extends StatelessWidget {
             ),
             // grid view
             Container(
-              color: Color(0xFFFFDAB9),
-              child: GridView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 5,
-                    crossAxisSpacing: 10,
-                    mainAxisExtent: MediaQuery.of(context).size.height * 0.30),
-                padding: EdgeInsets.all(10),
-                itemCount: data.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Card(
-                    color: Colors.white,
-                    shadowColor: Colors.black,
-                    elevation: 4,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Column(
-                      children: [
-                        Expanded(
-                          child: ClipRRect(
-                            child: Image.asset(data[index]['image'],
-                                fit: BoxFit.cover),
-                            borderRadius: BorderRadius.circular(5),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Column(
-                          //mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  data[index]['product'],
-                                  style: TextStyle(
-                                      color: Colors.black, fontSize: 14),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Text(
-                                  "/~~Rs.${data[index]['price']}  ",
-                                  style: TextStyle(
-                                      color: Colors.black, fontSize: 14),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                              height: 5,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                // ElevatedButton(
-                                //   onPressed: () {},
-                                //   child: Text(
-                                //     "Add to Cart",
-                                //     style: TextStyle(
-                                //       color: Colors.black,
-                                //     ),
-                                //   ),
-                                //   style: ElevatedButton.styleFrom(
-                                //       minimumSize: const Size(25, 30),
-                                //       backgroundColor: Color(0xFFFFDAB9)),
-                                // ),
-                                IconButton(
-                                  onPressed: () {},
-                                  icon: Icon(Icons.shopping_bag),
-                                ),
-                                // Icon(Icons.shopping_bag),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
+              color: Color(0xFFFFF0E1),
+              child: Productswidg(),
             ),
           ],
         ),
