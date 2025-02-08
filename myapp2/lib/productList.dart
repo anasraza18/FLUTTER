4b26d3d6e52bcec810930df956aad52355243c36
anasraza18 <manasraza18@gmail.com>
2025-02-08 14:41:11 +0500

    update

--- myapp2/lib/productList.dart
+++ myapp2/lib/productList.dart
@@ -27,87 +27,137 @@ class ProductList extends StatelessWidget {
               fontSize: 38, color: Colors.white, fontWeight: FontWeight.bold),
         ),
       ),
-      body: Container(
-        color: Color(0xFFFFDAB9),
-        child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              mainAxisSpacing: 5,
-              crossAxisSpacing: 10,
-              mainAxisExtent: MediaQuery.of(context).size.height * 0.30),
-          padding: EdgeInsets.all(10),
-          itemCount: data.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Card(
-              color: Colors.white,
-              shadowColor: Colors.black,
-              elevation: 4,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Column(
-                children: [
-                  Expanded(
-                    child: ClipRRect(
-                      child:
-                          Image.asset(data[index]['image'], fit: BoxFit.cover),
-                      borderRadius: BorderRadius.circular(5),
+      body: Column(
+        children: [
+          Column(
+            children: [
+              Container(
+                color: Color(0xFFFFDAB9),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      margin: EdgeInsets.all(4),
+                      //color: Colors.white,
+                      child: Icon(Icons.arrow_back),
                     ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Column(
-                    //mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            data[index]['product'],
-                            style: TextStyle(color: Colors.black, fontSize: 16),
-                          ),
-                        ],
+                    Container(
+                      padding: EdgeInsets.all(3),
+                      margin: EdgeInsets.all(4),
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage('assets/images/anas.jpg'),
                       ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Text(
-                            "Rs.${data[index]['price']}  ",
-                            style: TextStyle(color: Colors.black, fontSize: 16),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          ElevatedButton(
-                            onPressed: () {},
-                            child: Text(
-                              "Add to Cart",
-                              style: TextStyle(
-                                color: Colors.black,
-                              ),
-                            ),
-                            style: ElevatedButton.styleFrom(
-                                minimumSize: const Size(25, 30),
-                                backgroundColor: Color(0xFFFFDAB9)),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
+                    ),
+                  ],
+                ),
               ),
-            );
-          },
-        ),
+            ],
+          ),
+          Container(
+            color: Color(0xFFFFDAB9),
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  prefixIcon: Icon(Icons.search),
+                  labelText: "Search"),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Color(0xFFFFDAB9),
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 5,
+                    crossAxisSpacing: 10,
+                    mainAxisExtent: MediaQuery.of(context).size.height * 0.30),
+                padding: EdgeInsets.all(10),
+                itemCount: data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    color: Colors.white,
+                    shadowColor: Colors.black,
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: ClipRRect(
+                            child: Image.asset(data[index]['image'],
+                                fit: BoxFit.cover),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Column(
+                          //mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  data[index]['product'],
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 14),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Text(
+                                  "/~~Rs.${data[index]['price']}  ",
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 14),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                // ElevatedButton(
+                                //   onPressed: () {},
+                                //   child: Text(
+                                //     "Add to Cart",
+                                //     style: TextStyle(
+                                //       color: Colors.black,
+                                //     ),
+                                //   ),
+                                //   style: ElevatedButton.styleFrom(
+                                //       minimumSize: const Size(25, 30),
+                                //       backgroundColor: Color(0xFFFFDAB9)),
+                                // ),
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(Icons.shopping_bag),
+                                ),
+                                // Icon(Icons.shopping_bag),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
