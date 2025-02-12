dd6928234ec6eedb2f8253fa623386c5fc464559
anasraza18 <manasraza18@gmail.com>
2025-02-12 12:52:10 +0500

    update

--- myapp3/lib/counterapp.dart
+++ myapp3/lib/counterapp.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:myapp3/counterWeidget.dart';
+
+import 'package:myapp3/widgets/counterWeidget.dart';
 
 class Counterapp extends StatelessWidget {
   const Counterapp({super.key});
