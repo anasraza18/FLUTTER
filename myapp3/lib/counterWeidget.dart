d94d8744afa5adcf662aa5a536f4e8274fa0a544
anasraza18 <manasraza18@gmail.com>
2025-02-12 11:30:16 +0500

    update

--- myapp3/lib/counterWeidget.dart
+++ myapp3/lib/counterWeidget.dart
@@ -1,93 +1,93 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class Counterweidget extends StatefulWidget {
-//   const Counterweidget({super.key});
+class Counterweidget extends StatefulWidget {
+  const Counterweidget({super.key});
 
-//   @override
-//   State<Counterweidget> createState() => _CounterweidgetState();
-// }
+  @override
+  State<Counterweidget> createState() => _CounterweidgetState();
+}
 
-// class _CounterweidgetState extends State<Counterweidget> {
-//   int counter = 0;
-//   bool changecolor = false;
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       mainAxisAlignment: MainAxisAlignment.center,
-//       children: [
-//         Container(
-//           color: changecolor ? Colors.red : Colors.blue,
-//           height: 50,
-//           width: 50,
-//           child: Center(
-//             child: Text(
-//               "$counter",
-//               style: TextStyle(
-//                 fontSize: 32,
-//               ),
-//             ),
-//           ),
-//         ),
-//         SizedBox(
-//           height: 20,
-//         ),
-//         Row(
-//           mainAxisAlignment: MainAxisAlignment.spaceAround,
-//           children: [
-//             ElevatedButton(
-//               onPressed: () {
-//                 setState(() {
-//                   counter++;
-//                   changecolor = true;
-//                 });
-//               },
-//               child: Text(
-//                 "Add",
-//                 style: TextStyle(
-//                   color: Colors.white,
-//                 ),
-//               ),
-//               style: ElevatedButton.styleFrom(
-//                 backgroundColor: Colors.red,
-//               ),
-//             ),
-//             ElevatedButton(
-//               onPressed: () {
-//                 setState(() {
-//                   counter--;
-//                   changecolor = false;
-//                 });
-//               },
-//               child: Text(
-//                 "Minus",
-//                 style: TextStyle(
-//                   color: Colors.white,
-//                 ),
-//               ),
-//               style: ElevatedButton.styleFrom(
-//                 backgroundColor: Colors.blue,
-//               ),
-//             ),
-//             ElevatedButton(
-//               onPressed: () {
-//                 setState(() {
-//                   counter = 0;
-//                   changecolor = true;
-//                 });
-//               },
-//               child: Text(
-//                 "Reset",
-//                 style: TextStyle(
-//                   color: Colors.white,
-//                 ),
-//               ),
-//               style: ElevatedButton.styleFrom(
-//                 backgroundColor: Colors.red,
-//               ),
-//             ),
-//           ],
-//         ),
-//       ],
-//     );
-//   }
-// }
+class _CounterweidgetState extends State<Counterweidget> {
+  int counter = 0;
+  bool changecolor = false;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          color: changecolor ? Colors.red : Colors.blue,
+          height: 50,
+          width: 50,
+          child: Center(
+            child: Text(
+              "$counter",
+              style: TextStyle(
+                fontSize: 32,
+              ),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  counter++;
+                  changecolor = true;
+                });
+              },
+              child: Text(
+                "Add",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  counter--;
+                  changecolor = false;
+                });
+              },
+              child: Text(
+                "Minus",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  counter = 0;
+                  changecolor = true;
+                });
+              },
+              child: Text(
+                "Reset",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
--- myapp3/lib/home.dart
+++ myapp3/lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp3/counterapp.dart';
 import 'package:myapp3/widgets/products.dart';
 
 class Listofcounters extends StatelessWidget {
@@ -10,116 +11,168 @@ class Listofcounters extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.amber,
       ),
+      drawer: Drawer(
+        backgroundColor: Colors.amber,
+        child: ListView(
+          children: [
+            DrawerHeader(
+              child: Text(
+                "Fruits & Vegetables",
+                style: TextStyle(
+                  fontSize: 38,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text(
+                "  Home",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text(
+                "  Account",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text(
+                "  Counter",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Counterapp()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
       body: Container(
         color: Colors.amber,
         width: double.infinity,
         height: double.infinity,
         padding: EdgeInsets.only(left: 20, right: 20),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Home"),
-                CircleAvatar(
-                  backgroundImage: AssetImage("assets/images/anas.webp"),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Text(
-                  "Fruits  & ",
-                  style: TextStyle(
-                    fontSize: 34,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage("assets/images/anas.webp"),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Text(
-                  "vegetables",
-                  style: TextStyle(
-                    fontSize: 38,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-              ],
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 20),
-              height: 160,
-              width: 350,
-              child: ClipRRect(
-                child: Image.asset(
-                  "assets/images/banner.webp",
-                  fit: BoxFit.cover,
-                ),
-                borderRadius: BorderRadius.circular(8),
+                ],
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        "All",
-                        style: TextStyle(
-                          color: Colors.black,
-                        ),
-                      ),
+              Row(
+                children: [
+                  Text(
+                    "Fruits  & ",
+                    style: TextStyle(
+                      fontSize: 34,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
                     ),
-                    SizedBox(
-                      width: 20,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Text(
+                    "vegetables",
+                    style: TextStyle(
+                      fontSize: 38,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
                     ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Fruits",
-                        style: TextStyle(
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Vegetables",
-                        style: TextStyle(
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Mix",
-                        style: TextStyle(
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                  ],
+                  ),
+                ],
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 20),
+                height: 160,
+                width: 350,
+                child: ClipRRect(
+                  child: Image.asset(
+                    "assets/images/banner.webp",
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.circular(8),
                 ),
               ),
-            ),
-            homeWed(),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          "All",
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Fruits",
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Vegetables",
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Salat",
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              homeWed(),
+            ],
+          ),
         ),
       ),
     );
