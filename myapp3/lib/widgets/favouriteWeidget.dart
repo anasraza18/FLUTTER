148144a6b81916d8a5a4ab2d07d74ebbed39aed3
anasraza18 <manasraza18@gmail.com>
2025-02-12 13:19:25 +0500

    update

---
+++ myapp3/lib/widgets/favouriteWeidget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class favorite extends StatefulWidget {
+  const favorite({super.key});
+
+  @override
+  State<favorite> createState() => _favoriteState();
+}
+
+class _favoriteState extends State<favorite> {
+  bool isfav = false;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: IconButton(
+        onPressed: () {
+          setState(() {
+            isfav = !isfav;
+          });
+        },
+        icon: Icon(
+          Icons.favorite,
+          color: isfav ? Colors.red : Colors.white,
+        ),
+        iconSize: 32,
+      ),
+    );
+  }
+}
