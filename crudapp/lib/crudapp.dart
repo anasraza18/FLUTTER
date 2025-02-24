c3c3795d5728c7dfe49b645db27690ef20d5ecbb
anasraza18 <manasraza18@gmail.com>
2025-02-24 09:56:04 +0500

    update

--- crudapp/lib/crudapp.dart
+++ crudapp/lib/crudapp.dart
@@ -1,3 +1,4 @@
+import 'package:crudapp/data/data.dart';
 import 'package:crudapp/widget/listview.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +10,10 @@ class Crudapp extends StatefulWidget {
 }
 
 class _CrudappState extends State<Crudapp> {
+  TextEditingController numberr = TextEditingController();
+  TextEditingController titlee = TextEditingController();
+  TextEditingController subtitlee = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,16 +31,85 @@ class _CrudappState extends State<Crudapp> {
                   prefixIcon: Icon(Icons.search), hintText: "Search...."),
             ),
           ),
-          Container(
-            height: MediaQuery.sizeOf(context).height * 0.6,
+          Expanded(
             child: Listviewcrud(),
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          _showBottomSheet();
+        },
         child: Icon(Icons.add),
       ),
     );
   }
+
+  //--------------------bottomsheet-----------------------------------------
+  void _showBottomSheet() {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true, // agar full-screen bottom sheet chahiye
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      builder: (context) {
+        return Padding(
+          padding: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+            top: 16,
+            left: 16,
+            right: 16,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "Add New Item",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: numberr,
+                decoration: InputDecoration(
+                  labelText: "Enter Number...",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: titlee,
+                decoration: InputDecoration(
+                  labelText: "Enter title...",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: subtitlee,
+                decoration: InputDecoration(
+                  labelText: "Enter subtitle...",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {});
+                  data.add({
+                    'number': numberr.text,
+                    'title': titlee.text,
+                    'subtitle': subtitlee.text
+                  });
+                  Navigator.pop(context);
+                },
+                child: Text("Add"),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+  //---------------------------------------------------------------------
 }
--- crudapp/lib/widget/listview.dart
+++ crudapp/lib/widget/listview.dart
@@ -1,3 +1,4 @@
+import 'package:crudapp/data/data.dart';
 import 'package:flutter/material.dart';
 
 class Listviewcrud extends StatefulWidget {
@@ -8,30 +9,132 @@ class Listviewcrud extends StatefulWidget {
 }
 
 class _ListviewState extends State<Listviewcrud> {
-  List<Map<String, dynamic>> data = [];
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: 15,
+        itemCount: data.length,
         itemBuilder: (context, index) {
           return ListTile(
-            leading: Text("data"),
-            title: Text("data"),
-            subtitle: Text("data"),
+            leading: Text(data[index]['number']),
+            title: Text(data[index]['title']),
+            subtitle: Text(data[index]['subtitle']),
             trailing: Row(
               mainAxisSize: MainAxisSize.min,
               children: [
                 IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    _showMyDialog(index: index);
+                  },
                   icon: Icon(Icons.edit),
                 ),
                 IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    _showMyDialogRmv(index: index);
+                  },
                   icon: Icon(Icons.delete),
                 ),
               ],
             ),
           );
         });
+  }
+
+//-------------------show alert dialouge for edit--------------------------------------
+  void _showMyDialog({required int index}) {
+    TextEditingController editnum =
+        TextEditingController(text: data[index]['number']);
+    TextEditingController edittitle =
+        TextEditingController(text: data[index]['title']);
+    TextEditingController editsubtitle =
+        TextEditingController(text: data[index]['subtitle']);
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Dialog Title"),
+          content: Column(
+            children: [
+              TextFormField(
+                controller: editnum,
+                decoration: InputDecoration(
+                  labelText: "edit number...",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              TextFormField(
+                controller: edittitle,
+                decoration: InputDecoration(
+                  labelText: "edit title...",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              TextFormField(
+                controller: editsubtitle,
+                decoration: InputDecoration(
+                  labelText: "edit subtitle...",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Cancel"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                {
+                  setState(() {});
+                  data[index]['number'] = editnum.text;
+                  data[index]['title'] = edittitle.text;
+                  data[index]['subtitle'] = editsubtitle.text;
+                }
+                Navigator.pop(context);
+              },
+              child: Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  //-------------------show alert dialouge for edit--------------------------------------
+  void _showMyDialogRmv({required int index}) {
+    // TextEditingController editnum =
+    //     TextEditingController(text: data[index]['number']);
+    // TextEditingController edittitle =
+    //     TextEditingController(text: data[index]['title']);
+    // TextEditingController editsubtitle =
+    //     TextEditingController(text: data[index]['subtitle']);
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("are you sure !"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Cancel"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                {
+                  setState(() {});
+                  data.removeAt(index);
+                }
+                Navigator.pop(context);
+              },
+              child: Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
