8987eb53ed8a66f5eaf9e6590ccd7de5b5e12c17
anasraza18 <manasraza18@gmail.com>
2025-03-06 15:01:20 +0500

    update

---
+++ smitproject/lib/data/bestsellerdata.dart
@@ -0,0 +1,7 @@
+List bestsellerdata = [
+  {'image': "assets/images/1.webp", 'price': 123},
+  {'image': "assets/images/2.webp", 'price': 145},
+  {'image': "assets/images/3.webp", 'price': 221},
+  {'image': "assets/images/4.webp", 'price': 155},
+  {'image': "assets/images/5.webp", 'price': 344},
+];
--- smitproject/lib/widgets/bestseller.dart
+++ smitproject/lib/widgets/bestseller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smitproject/data/bestsellerdata.dart';
 import 'package:smitproject/data/catagorydata.dart';
 
 class Bestseller extends StatefulWidget {
@@ -12,33 +13,54 @@ class _BestsellerState extends State<Bestseller> {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 130,
+      height: 150,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: catogory.length,
+        itemCount: bestsellerdata.length,
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10),
             child: Column(
               children: [
-                Container(
-                  height: 70,
-                  width: 60,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    color: Color.fromARGB(255, 243, 233, 202),
-                  ),
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      catogory[index]['icon'],
-                      size: 40,
-                      color: Color.fromARGB(255, 236, 106, 45),
+                Stack(
+                  children: [
+                    Container(
+                      height: 150,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Color.fromARGB(255, 243, 233, 202),
+                        image: DecorationImage(
+                          image: AssetImage(bestsellerdata[index]['image']),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
                     ),
-                  ),
+                    Positioned(
+                      top: 108, // Move slightly up to overlap
+                      left: 30,
+                      right: 0,
+                      bottom: 20,
+                      child: Container(
+                        // height: 60,
+                        //width: 20,
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 236, 106, 45),
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(20),
+                                bottomLeft: Radius.circular(20))),
+                        child: Center(
+                            child: Text(
+                          "RS.${bestsellerdata[index]['price'].toString()}",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
+                        )),
+                      ),
+                    ),
+                  ],
                 ),
-                SizedBox(height: 5),
-                Text(catogory[index]['name'], style: TextStyle(fontSize: 16)),
               ],
             ),
           );
